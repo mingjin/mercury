b16fecfd5d96969afbe912d75ff5aebb6ff565aa
Ming Jin <skyairmj@gmail.com>
2016-09-09 17:39:36 +0800

    Ming Jin: implemented specs

--- spec/jd/supplier_fetcher_spec.rb
+++ spec/jd/supplier_fetcher_spec.rb
@@ -1,6 +1,15 @@
 describe SupplierFetcher do
   
-  it "fetch homepage" do
-    expect(subject.homepage).to eq('手机')
+  it "fetch mobile link" do
+    mobile_link = subject.fetch_mobile_link 'http://www.jd.com/allSort.aspx', '手机'
+    expect(mobile_link).to eq 'http://list.jd.com/list.html?cat=9987,653,655'
+  end
+  
+  it "fetch mobile merchants" do
+    merchants = subject.fetch_mobile_merchants 'http://list.jd.com/list.html?cat=9987,653,655', '手机'
+    expect(merchants.size).to be > 100
+    expect(merchants).to include '华为（HUAWEI）'
+    expect(merchants).to include 'LG'
+    expect(merchants).to include '谷歌（Google）'
   end
 end
