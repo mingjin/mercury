ed08130c1f3ea6a1afad1faf34b5affca867be79
Ming Jin <skyairmj@gmail.com>
2016-09-09 17:50:49 +0800

    Ming Jin: implemented with nokogiri

--- lib/mercury/jd/supplier_fetcher.rb
+++ lib/mercury/jd/supplier_fetcher.rb
@@ -3,8 +3,16 @@ require 'open-uri'
 
 class SupplierFetcher
   
-  def homepage
-    page = Nokogiri::HTML(open("http://www.jd.com/allSort.aspx"))
-    page.css('.category-item')[1].css('div.mc div.items dl dd a')[0].text
+  def fetch_mobile_link link, category
+    page = Nokogiri::HTML(open(link))
+    link = page.css('.category-item')[1].css('div.mc div.items dl dd a')[0]
+    link_protocol = link['href'].start_with?('//') ? 'http:' : ''
+    
+    link_protocol.concat link['href'] if link.text == category
+  end
+  
+  def fetch_mobile_merchants link, category
+    page = Nokogiri::HTML(open(link))
+    page.css('#brandsArea li a').map {|anchor| anchor['title']}
   end
 end
