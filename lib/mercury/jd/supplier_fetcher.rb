0086238f32bd8c05ecd4423eb8192afd51ca493d
Ming Jin <skyairmj@gmail.com>
2016-09-11 09:20:31 +0800

    Ming Jin: refactor browser away from fetchers

--- lib/mercury/jd/supplier_fetcher.rb
+++ lib/mercury/jd/supplier_fetcher.rb
@@ -1,36 +1,30 @@
-require 'selenium-webdriver'
-
 class SupplierFetcher
-    
+  
   def fetch_mobile_link link, category = '手机'
-    browser = Selenium::WebDriver.for :phantomjs
-    browser.navigate.to link
+    @browser.navigate.to link
     
-    div = browser.find_elements(:css, '.category-item')[1]
-    link = div.find_element(:css, 'div.mc div.items dl dd a')
+    div = @browser.find_elements(:css, '.category-item')[1]
+    anchor = div.find_element(:css, 'div.mc div.items dl dd a')
     
-    protocol_prefix = link.attribute('href').start_with?('//') ? 'http:' : ''
+    protocol_prefix = anchor.attribute('href').start_with?('//') ? 'http:' : ''
     
-    link_href = protocol_prefix.concat link.attribute('href')
-    link_href if link.text.eql? category
-    
-    browser.quit
+    link_href = protocol_prefix.concat anchor.attribute('href')
+    link_href if anchor.text.eql? category
     
     link_href
   end
   
   def fetch_mobile_merchants link, category = '手机'
-    browser = Selenium::WebDriver.for :phantomjs
-    browser.navigate.to link
+    @browser.navigate.to link
     
-    browser.find_element(:css, 'a.sl-e-more.J_extMore').click
+    @browser.find_element(:css, 'a.sl-e-more.J_extMore').click
     sleep 0.5
-    merchants = browser.find_elements(:css, 'ul#brandsArea li a').map {|anchor| anchor.attribute('title')}
-    
-    browser.quit
+    merchants = @browser.find_elements(:css, 'ul#brandsArea li a').map {|anchor| anchor.attribute('title')}
     
     merchants
   end
   
-  
+  def browser _browser
+    @browser = _browser
+  end
 end
--- spec/jd/supplier_fetcher_spec.rb
+++ spec/jd/supplier_fetcher_spec.rb
@@ -1,12 +1,24 @@
+require 'selenium-webdriver'
+
 describe SupplierFetcher do
+  before(:all) do
+    @fetcher = SupplierFetcher.new
+    @browser = Selenium::WebDriver.for :phantomjs
+    @fetcher.browser @browser
+  end
+  
+  after(:all) do
+    @browser.close
+    @browser.quit
+  end
   
   it "fetch mobile link" do
-    mobile_link = subject.fetch_mobile_link 'http://www.jd.com/allSort.aspx', '手机'
+    mobile_link = @fetcher.fetch_mobile_link 'http://www.jd.com/allSort.aspx', '手机'
     expect(mobile_link).to eq 'http://list.jd.com/list.html?cat=9987,653,655'
   end
   
   it "fetch mobile merchants" do
-    merchants = subject.fetch_mobile_merchants 'http://list.jd.com/list.html?cat=9987,653,655', '手机'
+    merchants = @fetcher.fetch_mobile_merchants 'http://list.jd.com/list.html?cat=9987,653,655', '手机'
     expect(merchants).to include '华为（HUAWEI）'
     expect(merchants).to include 'LG'
     expect(merchants).to include '谷歌（Google）'
